fb9372b1d6b1f697c831236a56b1c421b999b9be
Vitor Hugo Nakai <hugo.nakai@gmail.com>
2022-12-29 14:38:59 -0300

    Atualização

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 
 import {
   Box,
@@ -18,86 +17,152 @@ import {
   useBreakpointValue,
   useDisclosure,
   Heading,
-  Img,
+  Image,
 } from "@chakra-ui/react";
 
 //const vector = () => <Image src={"./Vector.svg"} width={280} height={60} />;
 
-const HOME = () => {
+function Menu() {
   const { isOpen, onToggle } = useDisclosure();
+  return (
+    <Box mt={8}>
+      <Flex
+        bg={"white"}
+        color={"#4F351E"}
+        minH={"60px"}
+        py={{ base: 2 }}
+        
+        align={"center"}
+        mt={8}
+        mb={8}
+      >
+        <Flex
+          flex={{ base: 1, md: "auto" }}
+          ml={{ base: -2 }}
+          display={{ base: "flex", md: "none" }}
+        >
+          <IconButton
+            onClick={onToggle}
+            icon={
+              isOpen ? (
+                <Text w={5} h={5}>
+                  X
+                </Text>
+              ) : (
+                <Text w={5} h={5}>
+                  #
+                </Text>
+              )
+            }
+            variant={"ghost"}
+            aria-label={"Alternar a navegação"}
+          />
+        </Flex>
+        <Flex
+          flex={{ base: 1 }}
+          justify={{ base: "center", md: "space-between" }}
+        >
+          <Image src="./Vector.svg" />
+          <Flex display={{ base: "none", md: "flex" }}>
+            <DesktopNav />
+          </Flex>
+        </Flex>
+
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={"flex-end"}
+          direction={"row"}
+          ml={8}
+        >
+          <Button
+            display={{ base: "none", md: "inline-flex" }}
+            fontSize={"sm"}
+            fontWeight={600}
+            color={"#4F351E"}
+            bg={"#EFF1B9"}
+            href={"#"}
+            _hover={{
+              bg: "#C7CA87",
+            }}
+          >
+            CONTATO
+          </Button>
+        </Stack>
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity>
+        <MobileNav />
+      </Collapse>
+    </Box>
+  );
+}
+
+function Hero() {
+  return (
+    <Stack direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"left"}>
+        <Stack spacing={8} w={"full"} maxW={"lg"}>
+          <Heading
+            fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}
+          >
+            <Text as={"span"} position={"relative"} color={"#4F351E"}>
+              filosofia clínica e <br />
+              coach de vida
+            </Text>
+            <br />{" "}
+            <Text
+              color={"#4F351E"}
+              as={"span"}
+              fontSize={"lg"}
+              fontWeight={200}
+            >
+              Não sei o suficiente
+            </Text>{" "}
+          </Heading>
+          <Text
+            fontSize={{ base: "md", lg: "lg" }}
+            color={"#1E1E1E"}
+            textAlign={"justify"}
+          >
+            Um resumo de duas linhas falando de forma generalista o que você
+            oferece (sem ser o seu corpo).
+          </Text>
+          <Stack direction={{ base: "column", md: "row" }}>
+            <Button
+              mt={8}
+              href={"#"}
+              fontWeight={600}
+              bg={"#4F351E"}
+              color={"white"}
+              _hover={{
+                bg: "#734B28",
+              }}
+            >
+              MARQUE UMA SESSÃO
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={"Login Image"}
+          objectFit={"cover"}
+          src={"./caleb-woods-iobrSsVqp28-unsplash.jpg"}
+        />
+      </Flex>
+    </Stack>
+  );
+}
+
+const HOME = () => {
   return (
     <>
       <Head>
         <title>Ivan Za</title>
       </Head>
-      <Box mt={8}>
-        <Flex
-          bg={"white"}
-          color={"#4F351E"}
-          minH={"60px"}
-          py={{ base: 2 }}
-          px={{ base: 4 }}
-          align={"center"}
-          m={8}
-        >
-          <Flex
-            flex={{ base: 1, md: "auto" }}
-            ml={{ base: -2 }}
-            display={{ base: "flex", md: "none" }}
-          >
-            <IconButton
-              onClick={onToggle}
-              icon={
-                isOpen ? (
-                  <Text w={5} h={5}>
-                    X
-                  </Text>
-                ) : (
-                  <Text w={5} h={5}>
-                    #
-                  </Text>
-                )
-              }
-              variant={"ghost"}
-              aria-label={"Alternar a navegação"}
-            />
-          </Flex>
-          <Flex
-            flex={{ base: 1 }}
-            justify={{ base: "center", md: "space-between" }}
-          >
-            <Img src="./Vector.svg" />
-            <Flex display={{ base: "none", md: "flex" }} ml={10}>
-              <DesktopNav />
-            </Flex>
-          </Flex>
-
-          <Stack
-            flex={{ base: 1, md: 0 }}
-            justify={"flex-end"}
-            direction={"row"}
-            spacing={6}
-            ml={8}
-          >
-            <Button
-              display={{ base: "none", md: "inline-flex" }}
-              fontSize={"sm"}
-              fontWeight={600}
-              color={"#4F351E"}
-              bg={"#EFF1B9"}
-              href={"#"}
-              _hover={{
-                bg: "#C7CA87",
-              }}
-            >
-              CONTATO
-            </Button>
-          </Stack>
-        </Flex>
-
-        <Collapse in={isOpen} animateOpacity>
-          <MobileNav />
-        </Collapse>
+      <Box margin={150} marginTop={0}>
+        <Menu />
+        <Hero />
       </Box>
 
       <Heading>trY</Heading>
