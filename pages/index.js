d3b98150008723bb951c028cf7d1c3b818b896f5
Vitor Hugo Nakai <89658457+whathebea@users.noreply.github.com>
2023-01-17 20:57:25 -0300

    logo 2

--- pages/index.js
+++ pages/index.js
@@ -113,7 +113,7 @@ function Menu() {
             <Image
               alt="Logo: Ivan Zanet Mangili - Coach Pessoal"
               maxW={{ base: "160px", md: "150px" }}
-              src="./logo_izm.png"
+              src="./logo_izm_2.png"
             />
           </a>
           <Flex display={{ base: "none", md: "flex" }} ml={10}>
