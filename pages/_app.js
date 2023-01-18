fef328d6e60a0edc0957b536b300e639dcf0520e
Vitor Hugo Nakai <89658457+whathebea@users.noreply.github.com>
2023-01-18 09:24:49 -0300

    Analytics reconfig

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import { ChakraProvider } from "@chakra-ui/react";
-import Analytics from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react';
 import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
